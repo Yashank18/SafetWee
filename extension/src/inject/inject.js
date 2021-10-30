158665d8cdb09e3cee75ccffd307a8b3277b9d21
Abhinav <abhinav.022000@gmail.com>
2021-10-31 02:34:53 +0530

    add dom color

--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -6,20 +6,19 @@ chrome.extension.sendMessage({}, function (response) {
 		document.querySelectorAll("article>div>div>div>div>div>div>div>div").forEach((ele) => {
 			if(ele.childNodes[0].nodeName === "SPAN"){
 				ele.querySelectorAll("span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0").forEach((r) => {
-				r.className = num;
+					r.className = num;
 			})};
 			num += 1;
 		});
-		for(var i = 101; i < num;i += 1) {
+		for(var i = 101; i < num; i += 1) {
 			var tweet = "";
-			document.getElementsByClassName(i).forEach((temp) => {
-				tweet += temp?.innerText
-			})
+			var abhinav = document.getElementsByClassName(i);
+			for (var j = 0; j < abhinav.length; j++) {
+				tweet += abhinav[j]?.innerText;
+			}
 			if(tweet.length !== 0) {
 				// const input = tweet;
 				// const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(input);
-				var abhinav = document.getElementsByClassName(i)
-
 				for (var j = 0; j < abhinav.length; j++) {
 					abhinav[j].style.color="blue";
 				}
