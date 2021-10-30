c068cf6e4c7c641cbb8aec01a50398fc6ddd10c7
Aravind Nair <22199259+aravindvnair99@users.noreply.github.com>
2021-10-31 00:27:56 +0530

    Add backend
    Signed-off-by: Aravind Nair <22199259+aravindvnair99@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express"),
+	app = express(),
+	path = __dirname + "/views/",
+	bodyParser = require("body-parser"),
+	morgan = require("morgan"),
+	{spawn} = require("child_process");
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: false,
+	})
+);
+app.use(express.static(__dirname + "/public"));
+app.set("views", path);
+app.set("view engine", "ejs");
+app.listen(process.env.PORT || 8000, () => {
+	console.info("App is running on port", process.env.PORT || 8000);
+});
+
+
+/*=============================================>>>>>
+
+				= Basic routes =
+
+===============================================>>>>>*/
+
+app.get("/", (_req, res) => {
+	res.render("index");
+});
+app.get("/download", (_req, res) => {
+	res.redirect("https://github.com/Yashank18/SafetWee/archive/main.zip");
+});
+app.post("/getRating", (req, res) => {
+	res.header(
+		"Access-Control-Allow-Origin",
+		"*"
+	);
+	let pythonOutput;
+	console.log("Username from extension is",req.body.username)
+	const python = spawn("python3", ["get_rating.py", req.body.username]);
+	python.stdout.on("data", (data) => {
+		pythonOutput = data.toString();
+	});
+	python.on("close", (code) => {
+		console.log(`Python exit code: ${code}`);
+		console.log(pythonOutput);
+		res.send(pythonOutput);
+	});
+});
+
+app.use((_req, res) => {
+	res.status(404).render("error", {
+		errorMessage: "404!",
+	});
+});
